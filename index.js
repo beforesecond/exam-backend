d8e30e04930414dd1a3907056c4980c196e7b3e2
Firstnaruto <firstnaruto@msn.com>
2020-05-26 18:12:38 +0700

    [🐛] Fix run prod

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('./src/index');
+/* eslint-disable global-require */
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line import/no-unresolved
+  require('./build/index')
+} else {
+  require('@babel/register')({ extensions: ['.js', '.ts'] })
+  require('./src/index')
+}
